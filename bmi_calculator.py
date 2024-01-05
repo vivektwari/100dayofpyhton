6c4cb64876c9c10848f0a309ce17b58bb30bddb4
Vivek Tiwari <155732958+vivektwari@users.noreply.github.com>
2024-01-05 16:59:14 +0530

    Add files via upload

---
+++ bmi_calculator.py
@@ -0,0 +1,11 @@
+
+
+#bmi project
+
+
+print("welcome t BMI calculator")
+
+weight = int(input("Enter your weight in kg "))
+height = float(input("Enter your height in meter "))
+bmi = weight / height **2
+print("your calculated bmi is",bmi)
