6495f5f83ae4d6bf9936cb4fb77ee8f09f0c2958
Vivek Tiwari <155732958+vivektwari@users.noreply.github.com>
2024-01-05 16:59:38 +0530

    Add files via upload

---
+++ elif_if_else.py
@@ -0,0 +1,15 @@
+print("TICKET FOR GAINT WHEEL ")
+
+height = int(input("Enter your height in cm "))
+if height >= 130:
+  age = int(input("Enter your age "))
+  if age <=12:
+    print("pay 50rs")
+  elif age <=18:
+    print("pay 100rs") 
+  elif age > 18:
+    print("pay 150rs please")
+else:
+    print("you cnt ride ")
+
+     
